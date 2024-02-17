1061e3b66e62b3ee8d3b058e8cae659834594e62
Amir Elaguizy <aelaguiz@gmail.com>
2024-02-17 10:19:43 -0600

    WIP

--- langdspy/test.py
+++ langdspy/test.py
@@ -1,7 +1,17 @@
 from langchain.prompts import BasePromptTemplate  # Assuming this is the correct import path
 from langchain.prompts import FewShotPromptTemplate
+from langchain_core.runnables import RunnableSerializable
 from langchain_core.pydantic_v1 import BaseModel, Field, create_model, root_validator
-from typing import Any, Dict, List, Type
+from langchain_core.pydantic_v1 import validator
+from typing import Any, Dict, List, Type, Optional
+from abc import ABC, abstractmethod
+from langchain_core.runnables.utils import (
+    Input,
+    Output
+)
+from langchain_core.runnables.config import (
+    RunnableConfig
+)
 
 class FieldDescriptor:
     def __init__(self, desc: str):
@@ -22,43 +32,77 @@ class InputField(FieldDescriptor):
 class OutputField(FieldDescriptor):
     pass
 
-class langdspySignature(BasePromptTemplate, BaseModel):
-    def __init_subclass__(cls, **kwargs):
-        super().__init_subclass__(**kwargs)
-        cls._collect_input_output_fields()
+class langdspyPromptTemplate(BasePromptTemplate, BaseModel):
+    # Assuming input_variables and output_variables are defined as class attributes
+    input_variables: List[str] = []
+    output_variables: List[str] = []
 
-    @classmethod
-    def _collect_input_output_fields(cls):
-        cls._inputs = {}
-        cls._outputs = {}
-        for name, attribute in cls.__dict__.items():
-            if isinstance(attribute, InputField):
-                cls._inputs[name] = attribute.desc
-            elif isinstance(attribute, OutputField):
-                cls._outputs[name] = attribute.desc
+    def __init__(self, **kwargs):
+        print("Init")
+        print(dir(self.__class__))
+        print("Calling super")
+        # Temporarily bypassing the setting of input and output variables
+        super().__init__(**kwargs)
+        print("Done super")
+        print(self.__class__.__fields__)
+
+        inputs = {}
+        outputs = {}
+
+        for name, attribute in self.__class__.__fields__.items():
+            print(f"field: {name}, value: {attribute}")
+
+            if attribute.type_ == InputField:
+                inputs[name] = attribute
+            elif attribute.type_ == OutputField:
+                outputs[name] = attribute
+
+        self.input_variables = list(inputs.keys())
+        self.output_variables = list(outputs.keys())
+
+        print(f"Inputs: {self.input_variables}")
+        print(f"Outputs: {self.output_variables}")
+
 
     def format_prompt(self, **kwargs: Any) -> str:
         prompt = ""
         for input_name in self.input_variables:
+            # print(f"input_name: {input_name}")
             prompt += f"{input_name}: {kwargs.get(input_name, 'N/A')}\n"
         return prompt
-
-    @classmethod
-    def get_input_schema(cls) -> Type[BaseModel]:
-        fields = {name: (str, Field(..., description=desc)) for name, desc in cls._inputs.items()}
-        return create_model(f'{cls.__name__}InputSchema', **fields)
 
     def format(self, **kwargs: Any) -> str:
         return self.format_prompt(**kwargs)
 
+        
+class langdspyPredict(RunnableSerializable):
+    template: langdspyPromptTemplate = None
+    
+    @validator("template")
+    def check_template(
+        cls, value: langdspyPromptTemplate
+    ) -> langdspyPromptTemplate:
+        print(f"Checking template: {value}")
+        return value
+    
+    def invoke(self, input: Input, config: Optional[RunnableConfig] = None) -> Output:
+        pass
 
-class GenerateAbsoluteHandStrength(langdspySignature):
+
+class GenerateAbsoluteHandStrength(langdspyPromptTemplate):
     player_hole_cards = InputField(desc="Description for player hole cards")
     board_cards = InputField(desc="Description for board cards")
     street = InputField(desc="Description for street")
     strength = OutputField(desc="Description for absolute hand strength")
 
 
+class HoleCardsCommentary(langdspyPredict):
+    absolute_hand_strength = langdspyPredict(template=GenerateAbsoluteHandStrength())
+    # def __init__(self):
+    #     self.absolute_hand_strength = langdspyPredict(template="hi")
+
+
 # FewShotPromptTemplate()
-model = GenerateAbsoluteHandStrength(input_variables=["player_hole_cards", "board_cards", "street"], output_variables=["strength"])
-print(model.__dict__)
+model = HoleCardsCommentary()
+print(model.absolute_hand_strength)
+# print(model.format())
