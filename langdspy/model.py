09a2a08a345d51cad1b23753c1989ad97f2eb88a
Amir Elaguizy <aelaguiz@gmail.com>
2024-02-17 15:13:16 -0600

    WIP

---
+++ langdspy/model.py
@@ -0,0 +1,171 @@
+from langchain.prompts import BasePromptTemplate  # Assuming this is the correct import path
+from langchain.prompts import FewShotPromptTemplate
+from langchain_core.runnables import RunnableSerializable
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field, create_model, root_validator, Extra
+from langchain_core.pydantic_v1 import validator
+from langchain_core.language_models import BaseLLM
+
+from typing import Any, Dict, List, Type, Optional
+from abc import ABC, abstractmethod
+from langchain_core.runnables.utils import (
+    Input,
+    Output
+)
+from langchain_core.runnables.config import (
+    RunnableConfig
+)
+from . import lcel_logger
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class FieldDescriptor:
+    def __init__(self, name:str, desc: str):
+        self.name = name
+        self.desc = desc
+
+    def __set_name__(self, owner, name):
+        self.private_name = f"_{name}"
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name, None)
+
+    def __set__(self, obj, value):
+        setattr(obj, self.private_name, value)
+
+class InputField(FieldDescriptor):
+    pass
+
+class OutputField(FieldDescriptor):
+    pass
+
+class Prediction(BaseModel):
+    class Config:
+        extra = Extra.allow  # This allows the model to accept extra fields that are not explicitly declared
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  # Initialize BaseModel with kwargs
+        for key, value in kwargs.items():
+            setattr(self, key, value)  # Dynamically assign attributes
+
+class PromptSignature(BasePromptTemplate, BaseModel):
+    # Assuming input_variables and output_variables are defined as class attributes
+    input_variables: Dict[str, Any] = []
+    output_variables: Dict[str, Any] = []
+
+    def __init__(self, **kwargs):
+        # print(dir(self.__class__))
+        # print("Calling super")
+        # Temporarily bypassing the setting of input and output variables
+        super().__init__(**kwargs)
+        # print("Done super")
+        # print(self.__class__.__fields__)
+
+        inputs = {}
+        outputs = {}
+
+        for name, attribute in self.__class__.__fields__.items():
+            # print(f"field: {name}, value: {attribute}")
+
+            if attribute.type_ == InputField:
+                inputs[name] = attribute.default
+            elif attribute.type_ == OutputField:
+                outputs[name] = attribute.default
+
+        self.input_variables = inputs
+        self.output_variables = outputs
+
+        # print(f"Inputs: {self.input_variables}")
+        # print(f"Outputs: {self.output_variables}")
+
+class PromptStrategy(BaseModel):
+    # def generate_prediction_return(self, **kwargs, output) -> Prediction:
+    #     pass
+    pass
+
+
+
+class DefaultPromptStrategy(PromptStrategy):
+    def format_prompt(self, **kwargs: Any) -> str:
+        self.validate_inputs(kwargs)
+
+        print("Formatting prompt")
+        print(f"Input variables: self.input_variables: {self.input_variables}")
+
+        prompt = "Follow the following format.\n\n"
+        for input_name, input_field in self.input_variables.items():
+            prompt += f"{input_field.name}: {input_field.desc}\n"
+
+        for output_name, output_field in self.output_variables.items():
+            prompt += f"{output_field.name}: {output_field.desc}\n"
+
+        prompt += "\n---\n\n"
+
+        for input_name, input_field in self.input_variables.items():
+            prompt += f"{input_field.name}: {kwargs.get(input_name)}\n"
+
+        prompt += f"{output_field.name}: "
+
+        return prompt
+
+    def validate_inputs(self, inputs_dict):
+        assert set(inputs_dict.keys()) == set(self.input_variables.keys()), "Input keys do not match expected input keys"
+
+    def format(self, **kwargs: Any) -> str:
+        return self.format_prompt(**kwargs)
+        
+class PromptRunner(RunnableSerializable):
+    template: PromptSignature = None
+
+    def __init__(self, template_class, prompt_strategy):
+        print(f"Initializing prompt runner")
+        super().__init__()
+        cls_ = type(template_class.__name__, (prompt_strategy, template_class), {})
+        self.template = cls_()
+    
+    @validator("template")
+    def check_template(
+        cls, value: PromptSignature
+    ) -> PromptSignature:
+        print(f"Checking template: {value}")
+        return value
+    
+    def invoke(self, input: Input, config: Optional[RunnableConfig] = None) -> Output:
+        # print(f"Prompt runner {self.template.__class__.__name__} invoked with input: {input}")
+        # prompt = self.template.format(**input)
+        # print(f"Prompt: {prompt}")
+
+        chain = (
+            self.template
+            | config['llm']
+            | StrOutputParser()
+        )
+
+        res = chain.invoke(input, config={'callbacks': []})
+        logger.debug(f"Result: {res}")
+
+        prediction_data = {**input}
+
+        for output_var_name in self.template.output_variables.keys():
+            prediction_data[output_var_name] = res
+
+        prediction = Prediction(**prediction_data)
+
+        return prediction
+
+
+
+    def __call__(self, **kwargs):
+        return self.invoke(kwargs)
+
+class Model(RunnableSerializable):
+    prompt_runners = []
+
+    def __init__(self):
+        super().__init__()
+
+        for field_name, field in self.__fields__.items():
+            if issubclass(field.type_, PromptRunner):
+                self.prompt_runners.append((field_name, field.default))
--- test.py
+++ test.py
@@ -29,11 +29,13 @@ from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model=os.getenv("FAST_OPENAI_MODEL"))
 
+logger = logging.getLogger(__name__)
+
 class GenerateAbsoluteHandStrength(langdspy.PromptSignature):
-    player_hole_cards = langdspy.InputField(name="Player Hole Cards", desc="Description for player hole cards")
-    board_cards = langdspy.InputField(name="Board Cards", desc="Description for board cards")
-    street = langdspy.InputField(name="Street", desc="Description for street")
-    strength = langdspy.OutputField(name="Hand Strength", desc="Description for absolute hand strength")
+    player_hole_cards = langdspy.InputField(name="Player Hole Cards", desc="The hole cards of the player.")
+    board_cards = langdspy.InputField(name="Board Cards", desc="The cards on the board.")
+    street = langdspy.InputField(name="Street", desc="The current street of the hand (one of: 'preflop', 'flop', 'turn', 'river')")
+    strength = langdspy.OutputField(name="Hand Strength", desc="The absolute strength of the player's hand based on the hole cards and board cards (Premium Hands (Top 3%), Strong Hands (Top 5%), Good Hands (Top 10%), Playable Hands, Marginal Hands, Lower-Tier Pairs, Offsuit Connectors and Gappers, Suited Gappers, Speculative Hands, Pair, Two Pair, Top Pair, etc)")
 
 
 class HoleCardsCommentary(langdspy.Model):
@@ -46,10 +48,10 @@ class HoleCardsCommentary(langdspy.Model):
 
 
 model = HoleCardsCommentary()
-res = model.invoke({
+pred = model.invoke({
     'player_hole_cards': 'Ah Kh',
     'board_cards': '2h 3h 4h',
     'street': 'flop'
 }, config={'llm': llm})
 
-print(f"Result: {res}")
+logger.info(f"Hand strength: {pred.strength}")
