6f09ca6ac31255df5620f67a9af006f6fb1c5f2d
Amir Elaguizy <aelaguiz@gmail.com>
2024-03-06 13:47:00 -0800

    Fixed error

--- langdspy/transformers.py
+++ langdspy/transformers.py
@@ -14,10 +14,6 @@ def as_json_list(val: str, kwargs: Dict[str, Any]) -> List[Dict[str, Any]]:
     return json.loads(val)
 
 def as_enum(val: str, kwargs: Dict[str, Any]) -> Enum:
-    print(f"Transforming {val} to enum")
-    enum_definition = kwargs['enum']
-
-def as_enum(val: str, kwargs: Dict[str, Any]) -> Enum:
     enum_class = kwargs['enum']
     try:
         return enum_class[val.upper()]
